31542c7f34bd95303a4a8b045c1ddff8c9ebb1ee
ivanmjartan <ivan.mjartan@gooddata.com>
2022-02-18 12:27:36 +0100

    Fix backend and redirect

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { WorkspaceProvider } from "../contexts/Workspace";
 import Page from "../components/Page";
